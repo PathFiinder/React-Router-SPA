d36f34bf8fbf16622a16e68519265b5b22f80804
Krystian <krystian_konecki@wp.pl>
2019-10-11 16:30:38 +0200

    Updates

---
+++ precache-manifest.fafb0b0ee1c205e09cae8f3a03fb4e51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367be1208eda28fc8f0ccbd1bcd7bcbd",
+    "url": "/React-Router-SPA/index.html"
+  },
+  {
+    "revision": "30bb73ecca0b08f99d68",
+    "url": "/React-Router-SPA/static/css/main.008b26fc.chunk.css"
+  },
+  {
+    "revision": "e6052aa71dab5eceb800",
+    "url": "/React-Router-SPA/static/js/2.63eed14e.chunk.js"
+  },
+  {
+    "revision": "30bb73ecca0b08f99d68",
+    "url": "/React-Router-SPA/static/js/main.7e0e815c.chunk.js"
+  },
+  {
+    "revision": "0640535415bd6b05d4e5",
+    "url": "/React-Router-SPA/static/js/runtime-main.67063106.js"
+  }
+]);
